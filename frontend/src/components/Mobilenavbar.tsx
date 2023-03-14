84a77eba2c4162fe3ee24b0a478e7c91370464c7
chosunguk <saeloun8797@gmail.com>
2023-03-14 17:45:03 +0900

    S08P22C205-202 merge

--- frontend/src/components/Mobilenavbar.tsx
+++ frontend/src/components/Mobilenavbar.tsx
@@ -3,7 +3,7 @@ import { HomeIcon, MapPinIcon, UserIcon, MapIcon  } from "@heroicons/react/24/ou
 
 const Mobilenavbar = () => {
   return (
-    <nav className="bg-gray-100 grid grid-cols-4 sticky bottom-0 justify-items-center">
+    <nav className="bg-gray-100 grid grid-cols-4 sticky bottom-0 justify-items-center py-3">
       <div className="">
         <HomeIcon className="w-6 h-6" />
       </div>
