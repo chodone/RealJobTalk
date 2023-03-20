2038cba87f0c27ea3d3fd9d5fddecc2bebe2505f
조승현 <sansusjc3@naver.com>
2023-03-20 10:08:21 +0900

    feat: change directory

---
+++ jobtalkbackend/src/test/java/com/ssafy/jobtalkbackend/JobtalkbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.jobtalkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobtalkbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
