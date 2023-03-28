213b645d4f6aa32a94d4e64033a6fed8160d42e1
조성욱 <saeloun8797@gmail.com>
2023-03-28 13:47:14 +0900

    S08P22C205-377 feat:로그인 page 구현

---
+++ frontend/src/pages/api/signincheck/callback.tsx
@@ -0,0 +1,80 @@
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import axios from "axios";
+
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   const { code } = req.query;
+
+//   if (code) {
+//     const url = "https://kauth.kakao.com/oauth/token";
+//     try {
+//       // 카카오 토큰 받기
+//       const { data } = await axios.post(url, null, {
+//         params: {
+//           grant_type: "authorization_code",
+//           client_id: `${process.env.REACT_APP_REST_API_KEY}`,
+//           redirect_uri: "http://localhost:3000/api/signincheck/callback",
+//           code,
+//         },
+//         headers: {
+//           "Content-Type": "application/x-www-form-urlencoded",
+//         },
+//       });
+//       const { access_token, refresh_token } = data;
+//       console.log(access_token)
+//       console.log(refresh_token)
+
+//       // // 사용자 정보 조회하기(이메일이랑 프로필 이미지 검색)
+//       // const infoResult = await axios.post(
+//       //   "https://kapi.kakao.com/v2/user/me",
+//       //   null,
+//       //   {
+//       //     headers: {
+//       //       Authorization: `Bearer ${access_token}`,
+//       //       "Content-Type": "application/x-www-form-urlencoded",
+//       //     },
+//       //   }
+//       // );
+//       // const email = infoResult.data.kakao_account.email;
+//       // const profileImage = infoResult.data.properties.profile_image;
+
+//     } catch {
+//       res.status(200).json({ error: "code가 없습니다." });
+//     }
+//   }
+// }
+
+
+
+'use client';
+
+import React, { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+import axios from "axios";
+
+
+export default function Home() {
+
+  const router = useRouter()
+  // const searchParams = useSearchParams()
+
+  useEffect(() => {
+    // const code = (router.asPath.split('?')[1] || '').split('=')[1];
+    // const code = searchParams?.get('code')
+    // console.log(code)
+    console.log(router)
+
+    axios.post(`http://localhost:8082/api/kakao/callback`, {
+      // code
+    })
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  })
+}
