2c0b5e51374e9b216f0bccbdded14512004b4eba
정민지 <wjdalswl0731@gmail.com>
2023-03-30 15:47:20 +0900

    hotfix

--- Crawling/crawlings/operator.py
+++ Crawling/crawlings/operator.py
@@ -5,7 +5,7 @@ from .views import naver_news_crawlling
 def start():
     scheduler=BackgroundScheduler()
 
-    @scheduler.scheduled_job('cron', hour=5, minute=5, id = 'crawling')
+    @scheduler.scheduled_job('cron', hour=6, minute=56, id = 'crawling')
     def auto_check():
         print('실행')
         naver_news_crawlling()
--- Crawling/crawlings/views.py
+++ Crawling/crawlings/views.py
@@ -8,7 +8,7 @@ import time
 
 def naver_news_crawlling():
     print('시작합니당')
-    client_hdfs = InsecureClient("http://172.17.0.4:29870", user="root")
+    client_hdfs = InsecureClient("http://localhost:29870", user="root")
     url = "https://openapi.naver.com/v1/search/news.json"
 
     headers = {
