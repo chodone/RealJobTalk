1279629ac7e5355a6dd4d605f39f170b0cdc5780
정민지 <wjdalswl0731@gmail.com>
2023-03-30 10:32:12 +0900

    S08P22C205-379 fix: crawling

--- Crawling/crawlings/operator.py
+++ Crawling/crawlings/operator.py
@@ -5,7 +5,7 @@ from .views import naver_news_crawlling
 def start():
     scheduler=BackgroundScheduler()
 
-    @scheduler.scheduled_job('cron', hour=17, minute=34, id = 'crawling')
+    @scheduler.scheduled_job('cron', hour=10, minute=36, id = 'crawling')
     def auto_check():
         print('실행')
         naver_news_crawlling()
