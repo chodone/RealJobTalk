32d8c74467f06c5b7ec0e6651c3cffc1ecdebc08
조성욱 <saeloun8797@gmail.com>
2023-03-30 15:24:21 +0900

    S08P22C205-377 feat:비밀번호 확인 기능 구현

--- frontend/src/components/SignUpForm.tsx
+++ frontend/src/components/SignUpForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import React, { FormEvent,useState } from "react";
+import React, { FormEvent,use,useState } from "react";
 import logo from "@public/images/logo.png";
 import { useRouter } from "next/navigation";
 import { useForm, useWatch, Control} from "react-hook-form";
@@ -11,6 +11,8 @@ interface FormData {
   email: string
   nickname: string
   password: string
+  passwordConfirm: string
+  message:string
 }
 
 
@@ -22,27 +24,61 @@ function EmailCheckButton({ control, setEmailAva }: { control: Control<FormData>
     defaultValue: "default"
   });
   const emailCheck = (e: FormEvent) => {
-    e.preventDefault()
+    e.preventDefault();
     fetch("http://localhost:8082/api/member/email/check", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
       },
-      body: JSON.stringify({email}),
+      body: JSON.stringify({ email }),
     })
-    .then((response) => response.json())
+      .then((response) => response.json())
       .then((data) => {
-      console.log(data)
-      if (data === true) {
-        console.log("Success:", data);
-        setEmailAva(true);
-      }
-      else if(data.status=== 400){
-        alert(data.message)
-      }
-  })
+        console.log(data);
+        if (data === true) {
+          console.log("Success:", data);
+          setEmailAva(true);
+        } else if (data.status === 400) {
+          alert(data.message);
+        }
+      });
+  }
+    
+      return (
+        <button onClick={(e) => {emailCheck(e)}} className="bg-green-500 text-white rounded-md px-1 my-2 text-xs float-right">중복체크</button>
+      )
     
   }
+
+  function NicknameCheckButton({ control, setNickAva }: { control: Control<FormData>, setNickAva:React.Dispatch<React.SetStateAction<boolean>> }) {
+    const nickname = useWatch({
+      control,
+      name: "nickname",
+      defaultValue: "nickname"
+    });
+    const emailCheck = (e: FormEvent) => {
+      e.preventDefault()
+      fetch("http://localhost:8082/api/member/email/check", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify({nickname}),
+      })
+      .then((response) => response.json())
+        .then((data) => {
+        console.log(data)
+        if (data === true) {
+          console.log("Success:", data);
+          setNickAva(true);
+        }
+        else if(data.status=== 400){
+          alert(data.message)
+        }
+    })
+      
+    }
+  
 
   return (
     <button onClick={(e) => {emailCheck(e)}} className="bg-green-500 text-white rounded-md px-1 my-2 text-xs float-right">중복체크</button>
@@ -54,6 +90,7 @@ function EmailCheckButton({ control, setEmailAva }: { control: Control<FormData>
 
 const SignUphtmlForm = () => {
   const [emailAva, setEmailAva] = useState(false)
+  const [nickAva, setNickAva] = useState(false)
 
   const MainLogo = logo;
   const router = useRouter()
@@ -100,7 +137,7 @@ const SignUphtmlForm = () => {
                     autoComplete="off"
                     aria-invalid = {errors.email ? "true" : "false"}
                     id="email"
-                    type="text"
+                    type="text"           
                     className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
                     placeholder="Email address"
                     {...register("email", {
@@ -113,8 +150,9 @@ const SignUphtmlForm = () => {
                         message: "Email 형식이 맞지 않습니다.",
                       },
                     })}
+                    onChange={() => {setEmailAva(false)}}
                   />
-                  {!emailAva && <EmailCheckButton control={control} setEmailAva={setEmailAva} />}
+                  {!emailAva ? <EmailCheckButton control={control} setEmailAva={setEmailAva} /> : <p className="text-green-700 text-xs"> 사용가능한 Email 입니다. </p>}
                   <label
                     htmlFor="email"
                     className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
@@ -130,13 +168,14 @@ const SignUphtmlForm = () => {
                         message: "닉네임을 입력해주세요.",
                       },
                     })}
+                    onChange={() => {setNickAva(false)}}
                     autoComplete="off"
                     id="nickname"
                     type="text"
                     className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
                     placeholder="Nickname"
                   />
-                  <button className="bg-green-500 text-white rounded-md px-1 my-2 text-xs float-right ">중복체크</button>
+                    {!nickAva ? <NicknameCheckButton control={control} setNickAva={setNickAva} /> : <p className="text-green-700 text-xs"> 사용가능한 Nickname 입니다. </p>}
                   <label
                     htmlFor="nickname"
                     className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
@@ -169,21 +208,32 @@ const SignUphtmlForm = () => {
 
                 <div className="relative">
                   <input
-                    {...register("password", {
+                    autoComplete="off"
+                    id="passwordConfirm"
+                    
+                    type="password"
+                    aria-invalid={errors?.passwordConfirm ? "true" : "false"}
+                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                    placeholder="Password"
+                    {...register("passwordConfirm", {
                       required: {
                         value: true,
                         message: "비밀번호를 입력해주세요.",
                       },
+                      // pattern: {
+                      //   value: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$/,
+                      //   message: "비밀번호는 대문자, 소문자, 특수문자, 숫자를 포함해야합니다 (8~16 글자)",
+                      // },
+                      validate: (value) => {
+                        if (value != watch("password")) {
+                          return "비밀번호가 일치하지 않습니다.";
+                        }
+                      },
                     })}
-                    autoComplete="off"
-                    id="password"
-                    name="password"
-                    type="password"
-                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
-                    placeholder="Password"
                   />
+                  {errors && <p className="text-red-500 text-xs">{errors.passwordConfirm?.message}</p>}
                   <label
-                    htmlFor="password"
+                    htmlFor="passwordConfirm"
                     className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
                   >
                     Password Confirm
