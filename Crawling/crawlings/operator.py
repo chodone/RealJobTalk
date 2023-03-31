bc9c017853205e4fc45d29f76b1c29cc5a8ca4d0
정민지 <wjdalswl0731@gmail.com>
2023-03-31 11:30:28 +0900

    hotfix

--- Crawling/crawlings/operator.py
+++ Crawling/crawlings/operator.py
@@ -1,16 +1,14 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from .views import naver_news_crawlling
 from .views import test
-import time
 
 
 def start():
     scheduler=BackgroundScheduler()
 
-    @scheduler.scheduled_job('cron', hour=2, minute=8, id = 'crawling')
+    @scheduler.scheduled_job('cron', hour=2, minute=38, id = 'crawling')
     def auto_check():
         print('실행')
-        test()
-        time.sleep(3)
-        # naver_news_crawlling()
+        # test()
+        naver_news_crawlling()
     scheduler.start()
--- Crawling/crawlings/views.py
+++ Crawling/crawlings/views.py
@@ -61,4 +61,5 @@ def naver_news_crawlling():
 
                         value = enterprise.strip() + ('\n') + today + ('\n') + jsonIdx['link'] + ('\n') + cleantext + ('\n')  + ('\n') + content[0].text
 
+                        time.sleep(3)
                         client_hdfs.write(f'/user/root/test/{filename}.txt', data=value, overwrite=True, encoding="UTF8")
