e4fa75cf90f3fcddd1d5d3f9ec457f0aeb54876c
정민지 <wjdalswl0731@gmail.com>
2023-03-31 10:29:56 +0900

    hotfix

--- Crawling/crawlings/operator.py
+++ Crawling/crawlings/operator.py
@@ -1,12 +1,14 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from .views import naver_news_crawlling
+import time
 
 
 def start():
     scheduler=BackgroundScheduler()
 
-    @scheduler.scheduled_job('cron', hour=1, minute=20, id = 'crawling')
+    @scheduler.scheduled_job('cron', hour=1, minute=35, id = 'crawling')
     def auto_check():
         print('실행')
+        time.sleep(3)
         naver_news_crawlling()
     scheduler.start()
