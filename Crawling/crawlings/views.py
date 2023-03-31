18ec8ee1e7200d8f1a3d1d82d1882b8d214aca38
정민지 <wjdalswl0731@gmail.com>
2023-03-31 10:06:58 +0900

    hotfix

--- Crawling/crawlings/views.py
+++ Crawling/crawlings/views.py
@@ -56,4 +56,4 @@ def naver_news_crawlling():
 
                         value = enterprise.strip() + ('\n') + today + ('\n') + jsonIdx['link'] + ('\n') + cleantext + ('\n')  + ('\n') + content[0].text
 
-                        client_hdfs.write(f'/user/root/test/{filename}.txt', data=value, overwrite=True, encoding="UTF8")
+                        client_hdfs.write(f'/user/root/test/{filename}.txt', data=value, overwrite=True)
