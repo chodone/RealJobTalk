3a2f9144bd018fde4d3800bb1953a90d9a6971d3
이재훈 <dlwogns1193@naver.com>
2023-03-31 17:57:15 +0900

    캐러셀 추가

--- frontend/src/components/Company.tsx
+++ frontend/src/components/Company.tsx
@@ -1,24 +1,26 @@
-'use client'
-import React from 'react'
-import Image from 'next/image';
-import Link from 'next/link';
+"use client";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-// export interface dbObject {
-//   company: string;
-//   url: string;
-// }
-
-
-const Company = ({ company }: { company: string }) => {
-  const imgurl = company['imgUrl']
-  const companyName = company['name'];
-  console.log(company)
-  return (
-    <Link className="transform h-64 w-64 transition duration-500 hover:scale-125" href={`${companyName}/info`}>
-      <Image  src={imgurl} alt=""  width={200} height={200}/>
-      <div>{companyName}</div>
-    </Link>
-  )
+export interface dbObject {
+  name: string;
+  imgUrl: string;
 }
 
-export default Company
+const Company = ({ company }: { company: dbObject }) => {
+  const imgurl = company["imgUrl"];
+  const companyName = company["name"];
+  console.log(company);
+  return (
+    <Link
+      className="transform h-64 w-64 transition duration-500 hover:scale-125"
+      href={`${companyName}/info`}
+    >
+      <Image src={imgurl} alt="" width={200} height={200} />
+      <div>{companyName}</div>
+    </Link>
+  );
+};
+
+export default Company;
