a1b3ab39d883300985a51c44a15325899fa3d515
정민지 <wjdalswl0731@gmail.com>
2023-04-03 11:59:07 +0900

    fix: crawling

--- Crawling/teset.py
+++ Crawling/teset.py
@@ -46,7 +46,88 @@ options.add_argument("--single-process")
 options.add_argument("--disable-dev-shm-usage")
 path='../chromedriver'
 
-aTag = "https://dogfootja.tistory.com/44"
+browser = webdriver.Chrome(path, options=options) #"./chromedriver.exe"
+browser.implicitly_wait(30)
+browser.maximize_window()
+
+enterprise_id = 0
+for enterprise in lines:
+    time.sleep(3)
+    browser.get("https://www.google.com/search?q="+enterprise.strip()+" 합격 후기 site:tistory.com")
+    
+    count = 1
+    for idx in range(300):
+        time.sleep(3)
+        print(enterprise.strip(), count)
+
+        aTag = ''
+        if count == 10:
+            aTag = browser.find_element(By.XPATH,'//*[@id="rso"]/div[10]/div/div/div/div[1]/div/a').get_attribute('href')
+            count = 0
+            browser.find_element(By.XPATH, '//*[@id="pnnext"]').click()
+        else:
+            aTag = browser.find_element(By.XPATH, f'//*[@id="rso"]/div[{count}]/div/div/div[1]/div/a').get_attribute('href')
+        
+        print(aTag)
+        count += 1
+
+        params = {
+            "access_token" : "a9356d05163a6af4030ca9dd8a68da40_136d9085af9fefc038f15b14124717eb",
+            "blogName" : aTag.split('/')[2].split('.')[0],
+            "postId" : aTag.split('/')[-1]
+        }
+
+        print()
+        res = requests.get(url, headers=headers, params=params)
+        print()
+        print(params)
+        print()
+        print("&&&&&&&&&&&&&&&&&&&&&&")
+        print("&&&&&&&&&&&&&&&&&&&&&&")
+        print(res)
+        soup = BeautifulSoup(res.text, "lxml")
+        print(soup)
+
+        root = soup.find("tistory")
+        print('---')
+        print(root)
+        
+
+        dateOfIssue = "".join(root.find("item").find("date").text.split(' ')[0].split('-'))
+        print(dateOfIssue)
+
+        if int(dateOfIssue[:4]) > 2019:
+            title =  root.find("item").find("title").text
+            url = root.find("item").find("url").text
+            cleantext = root.find("item").find("content").text.strip()
+            
+            filename = dateOfIssue+"_tistory_review_"+enterprise.strip()+"_"+str(idx+1)
+            value = enterprise.strip() + ('\n') + dateOfIssue + ('\n') + url + ('\n') + title + ('\n') + cleantext
+            print('hdfs 전송완료')
+            cursor = conn_aws.cursor()
+
+            selectSql = "SELECT MAX(pass_review_id) FROM pass_review"
+            cursor.execute(selectSql)
+            maxNewsId = cursor.fetchall()
+            maxNewsId = maxNewsId[0][0]
+            if maxNewsId == None:
+                maxNewsId = -1
+
+            conn_aws.commit()
+
+            sql = "INSERT INTO pass_review VALUES (%s, %s, %s, %s, %s, %s)"
+            value = (maxNewsId+1, dateOfIssue, title, url, enterprise_id, cleantext)
+            cursor.execute(sql, value)
+
+            conn_aws.commit()
+            print('mysql 전송완료')
+    enterprise_id += 1
+conn_aws.close()
+
+
+
+
+aTag = "https://gusdnr69.tistory.com/298"
 params = {
     "access_token" : 'a9356d05163a6af4030ca9dd8a68da40_136d9085af9fefc038f15b14124717eb',
     "blogName" : aTag.split('/')[2].split('.')[0],
