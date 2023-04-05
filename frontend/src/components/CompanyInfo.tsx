3c1763e2582b67db2d8c213e79db140828432b87
이재훈 <dlwogns1193@naver.com>
2023-04-06 00:30:07 +0900

    S08P22C205-480 feat: info

--- frontend/src/components/CompanyInfo.tsx
+++ frontend/src/components/CompanyInfo.tsx
@@ -8,6 +8,7 @@ import CompanyImage from "./CompanyImage";
 import UrlButtons from "./UrlButtons/UrlButtons";
 import { render } from 'react-dom';
 import WordCloud from 'react-d3-cloud';
+import Tooltip from '@/components/Tooltip'
 
 import InfoButtons from "./InfoButtons";
 
@@ -28,6 +29,7 @@ interface Data{
 interface Words {
   text: string;
   value: number;
+
 }
 
 
@@ -74,7 +76,10 @@ const CompanyInfo = ({ company, enterpriseId }: { company: string; enterpriseId:
       });
   }, []);
 
-  console.log(wordCount[0])
+  const onWordMouseOver = useCallback((word:Words) => {
+    return (<div className="transform transition duration-500 hover:scale-110">{word.text}</div>);
+  }, []);
+
 
   return (
     <div className="grid grid-rows-7  mx-36">
@@ -92,23 +97,24 @@ const CompanyInfo = ({ company, enterpriseId }: { company: string; enterpriseId:
         <div className=" grid grid-cols-2  mt-4 ml-4 divide-x">
           <div className="justify-center">
             <div className="flex justify-center">
-              <div className=" w-48 border border-[#02E86E] mb-3 text-center rounded-lg">
+              <div className=" w-48 border-2 border-[#02E86E] mb-3 text-center rounded-lg font-bold">
                 기업개요
               </div>
             </div>
-            <div>
-              <p>{companyInfo_.businessInformation}</p>
+            <div className="grid justify-items-center align-items-center">
+              <p className="m-10 mt-40">{companyInfo_.businessInformation}</p>
             </div>
           </div>
 
           <div className="justify-center">
             <div className="flex justify-center">
-              <div className="w-48 border border-[#02E86E] mb-3 text-center rounded-lg">
+              <div className="w-48 border-2 border-[#02E86E] mb-3 text-center rounded-lg font-bold">
                 워드클라우드
               </div>
             </div>
             <div>
-              <WordCloud data={wordCount} />
+              <WordCloud data={wordCount}
+               />
             </div>
           </div>
         </div>
