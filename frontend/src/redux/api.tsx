afd5651c45df8fb6cefa7d554d91e3ba1e7982b6
조성욱 <saeloun8797@gmail.com>
2023-04-06 01:03:58 +0900

    S08P22C205-480 base url 수정

--- frontend/src/redux/api.tsx
+++ frontend/src/redux/api.tsx
@@ -3,8 +3,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: "https://j8c205.p.ssafy.io",
-  baseURL : "http://localhost:8082"
+  baseURL: "https://j8c205.p.ssafy.io",
+  // baseURL : "http://localhost:8082"
 })
 
 export default api;
