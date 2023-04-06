e968d25c8b36d3271ce82eeede442a0180ed0c5a
조승현 <sansusjc3@naver.com>
2023-04-06 10:10:40 +0900

    S08P22C205-479 hotfix: kakaoLogin2

--- frontend/src/redux/api.tsx
+++ frontend/src/redux/api.tsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: "https://j8c205.p.ssafy.io",
-  // baseURL : "http://localhost:8082"
+  // baseURL: "http://localhost:8082"
 })
 
 export default api;
