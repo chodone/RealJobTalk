00d76613bc19b746dd079e11ebd61c8389a58753
조성욱 <saeloun8797@gmail.com>
2023-04-06 10:55:56 +0900

    S08P22C205-480 feat: kakao redirect 설정 완료

--- frontend/src/components/KakaoReturn.tsx
+++ frontend/src/components/KakaoReturn.tsx
@@ -4,10 +4,11 @@
 import React, { useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import api from "@/redux/api";
+import logo2 from '@public/images/logo2.png'
 import { useAppSelector, useAppDispatch } from '@/redux/hook'
 import { authActions } from "@/redux/reducer/authReducer";
 import jwtDecode from "jwt-decode";
-
+import Image from "next/image";
 
 
 
@@ -37,8 +38,15 @@ export default function KaKaoReturn() {
   })
 
   return (
-    <div>
-      callback
+    <div className="absolute right-1/2 bottom-1/2  transform translate-x-1/2 translate-y-1/2 justify-center">
+      
+      <div className="border-t-transparent border-solid animate-spin  rounded-full border-green-400 border-8 h-64 w-64">
+      </div>
+      <div className="flex flex-row">
+        <Image className=" ml-7" src={logo2} width={61} height={69} alt="" />
+        <p className="mt-5"> 카카오 로그인 중 ...</p>
+
+      </div>
     </div>
-  )
+  );
 }
